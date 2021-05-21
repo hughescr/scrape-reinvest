ebca7a2a04bea8773d061c5ce33c3c577d5bcf7f
Craig R. Hughes <craig.git@rungie.com>
2021-05-20 17:05:12 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,366 @@
+'use strict';
+
+const _ = require('lodash');
+const { DateTime } = require('luxon');
+const { promisify } = require('util');
+
+const nconf = require('nconf');
+
+const yargs = require('yargs')
+.version()
+.strict()
+.usage('$0 [-x]')
+.options({
+    x : {
+        alias: 'execute',
+        describe: 'Execute the withdrawls of accumulated rewards; without this option, the script will run read-only',
+        type: 'boolean',
+        'default': false,
+    }
+})
+.help();
+
+nconf.argv(yargs)
+    .env()
+    .file({ file: 'scrape-reinvest.json ' });
+
+const Web3 = require('web3');
+const web3 = new Web3('https://rpc-mainnet.maticvigil.com');
+
+const keychain = require('keychain');
+const getPassword = promisify(keychain.getPassword).bind(keychain);
+
+let MY_ACCOUNT_ID;
+
+const WMATIC_TOKEN =     '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270';
+const USDT_TOKEN   =     '0xc2132D05D31c914a87C6611C10748AEb04B58e8F';
+const USDC_TOKEN   =     '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174';
+const DAI_TOKEN    =     '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063';
+const amWBTC_TOKEN =     '0x5c2ed810328349100A66B82b78a1791B101C9D61';
+const amAAVE_TOKEN =     '0x1d2a0E5EC8E5bBDCA5CB219e649B565d8e5c3360';
+const amWMATIC_TOKEN =   '0x8df3aad3a84da6b69a4da8aec3ea40d9091b2ac4';
+const amWETH_TOKEN =     '0x28424507fefb6f7f8e9d3860f56504e4e5f5f390';
+const amUSDT_TOKEN =     '0x60D55F02A771d515e077c9C2403a1ef324885CeC';
+const amDebtUSDT_TOKEN = '0x8038857fd47108a07d1f6bf652ef1cbec279a2f3';
+const curveLP_TOKEN =    '0xE7a24EF0C5e95Ffb0f6684b813A78F2a3AD7D171';
+
+const erc20ContractABI = require('./erc20-contract.json');
+const WMATICContract = new web3.eth.Contract(erc20ContractABI, WMATIC_TOKEN);
+const USDTContract = new web3.eth.Contract(erc20ContractABI, USDT_TOKEN);
+const USDCContract = new web3.eth.Contract(erc20ContractABI, USDC_TOKEN);
+const DAIContract = new web3.eth.Contract(erc20ContractABI, DAI_TOKEN);
+const curveLPContract = new web3.eth.Contract(erc20ContractABI, curveLP_TOKEN);
+
+const curveGaugeABI = require('./curve-gauge-contract.json');
+const curveGaugeAddress = '0xe381C25de995d62b453aF8B931aAc84fcCaa7A62';
+const curveGaugeContract = new web3.eth.Contract(curveGaugeABI, curveGaugeAddress);
+
+const curvePoolABI = require('./curve-pool-contract.json');
+const curvePoolAddress = '0x445FE580eF8d70FF569aB36e80c647af338db351';
+const curvePoolContract = new web3.eth.Contract(curvePoolABI, curvePoolAddress);
+
+const aaveIncentivesControllerABI = require('./aave-contract.json');
+const aaveIncentivesControllerAddress = '0x357D51124f59836DeD84c8a1730D72B749d8BC23';
+const aaveIncentivesControllerContract = new web3.eth.Contract(aaveIncentivesControllerABI, aaveIncentivesControllerAddress);
+
+const sushiRouterABI = require('./sushiswap-contract.json');
+const sushiRouterAddress = '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506';
+const sushiRouterContract = new web3.eth.Contract(sushiRouterABI, sushiRouterAddress);
+
+const claimCurve = async (claim = true) => {
+    const rewards = await curveGaugeContract.methods
+                    .claimable_reward(MY_ACCOUNT_ID, WMATIC_TOKEN)
+                    .call();
+    console.log(`Will claim ${web3.utils.fromWei(rewards)} WMATIC for ${MY_ACCOUNT_ID} from Curve...`);
+
+    if(claim) {
+        return new Promise((resolve, reject) => {
+            curveGaugeContract.methods
+                .claim_rewards()
+                .send()
+            .on('transactionHash', hash => {
+                console.log(`Curve claim Tx hash: '${hash}'`);
+            })
+            .on('confirmation', (number, receipt) => {
+                if(number == 0) { console.log(`Curve claim confirmation number: ${number}`); }
+            })
+            .on('receipt', receipt => {
+                resolve(rewards);
+            })
+            .on('error', (err, receipt) => {
+                reject(err);
+            });
+        });
+    }
+
+    return rewards;
+};
+
+const claimAave = async (claim = true) => {
+    const rewards = await aaveIncentivesControllerContract.methods
+                        .getRewardsBalance([amWBTC_TOKEN, amWETH_TOKEN, amAAVE_TOKEN, amWMATIC_TOKEN, amDebtUSDT_TOKEN, amUSDT_TOKEN], MY_ACCOUNT_ID)
+                        .call();
+    console.log(`Will claim ${web3.utils.fromWei(rewards)} WMATIC for ${MY_ACCOUNT_ID} from AAVE...`);
+
+    if(claim) {
+        return new Promise((resolve, reject) => {
+            aaveIncentivesControllerContract.methods
+                .claimRewards([amWBTC_TOKEN, amWETH_TOKEN, amAAVE_TOKEN, amWMATIC_TOKEN, amDebtUSDT_TOKEN, amUSDT_TOKEN], rewards, MY_ACCOUNT_ID)
+                .send()
+            .on('transactionHash', hash => {
+                console.log(`AAVE Tx hash: '${hash}'`);
+            })
+            .on('confirmation', (number, receipt) => {
+                if(number == 0) { console.log(`AAVE confirmation number: ${number}`); }
+            })
+            .on('receipt', receipt => {
+                resolve(rewards);
+            })
+            .on('error', (err, receipt) => {
+                reject(err);
+            });
+        });
+    }
+
+    return rewards;
+};
+
+const swapToStablecoin = async (extra_wmatic, claim = true) => {
+    const WMATIC_decimals = await WMATICContract.methods.decimals().call();
+    const WMATIC_fromWei = _.findKey(web3.utils.unitMap,
+                            v => v == (new web3.utils.BN(10).pow(new web3.utils.BN(WMATIC_decimals))).toString());
+
+    const account_wmatic = await WMATICContract.methods.balanceOf(MY_ACCOUNT_ID).call();
+    if(!claim) {
+        console.log(`Account has balance of ${web3.utils.fromWei(account_wmatic, WMATIC_fromWei)} WMATIC`);
+    }
+    const wmatic = claim ? new web3.utils.BN(account_wmatic)
+                         : (new web3.utils.BN(account_wmatic)).add(new web3.utils.BN(extra_wmatic));
+    console.log(`Total of ${web3.utils.fromWei(wmatic, WMATIC_fromWei)} WMATIC tokens`);
+
+    const USDT_decimals = await USDTContract.methods.decimals().call();
+    const USDT_fromWei = _.findKey(web3.utils.unitMap,
+                            v => v == (new web3.utils.BN(10).pow(new web3.utils.BN(USDT_decimals))).toString());
+
+    const USDC_decimals = await USDCContract.methods.decimals().call();
+    const USDC_fromWei = _.findKey(web3.utils.unitMap,
+                            v => v == (new web3.utils.BN(10).pow(new web3.utils.BN(USDC_decimals))).toString());
+
+    const DAI_decimals = await DAIContract.methods.decimals().call();
+    const DAI_fromWei = _.findKey(web3.utils.unitMap,
+                            v => v == (new web3.utils.BN(10).pow(new web3.utils.BN(DAI_decimals))).toString());
+
+    const lp_decimals = await curveLPContract.methods.decimals().call();
+    const lp_fromWei = _.findKey(web3.utils.unitMap,
+                            v => v == (new web3.utils.BN(10).pow(new web3.utils.BN(lp_decimals))).toString());
+
+    const coin_indices = _.range(3);
+    const underlying_coins = await Promise.all(_.map(coin_indices, i => curvePoolContract.methods.underlying_coins(i).call()));
+
+    const currency_token_map = {
+        USDC: USDC_TOKEN,
+        USDT: USDT_TOKEN,
+        DAI: DAI_TOKEN,
+    };
+
+    const currency_contract_map = {
+        USDC: USDCContract,
+        USDT: USDTContract,
+        DAI: DAIContract,
+    };
+
+    const currency_fromWei_map = {
+        USDC: USDC_fromWei,
+        USDT: USDT_fromWei,
+        DAI: DAI_fromWei,
+    };
+
+    const underlying_coin_indices = _.mapValues(currency_token_map, c => _.findIndex(underlying_coins, x => x == c));
+
+
+    const amount_out = {};
+    const amount_out_lp = {};
+
+    await Promise.all(_.map(currency_token_map, async (token, coin) => {
+        amount_out[coin] = (await sushiRouterContract.methods
+                                    .getAmountsOut(wmatic, [WMATIC_TOKEN, token])
+                                    .call())[1];
+        const currency_in = ['0', '0', '0'];
+        currency_in[underlying_coin_indices[coin]] = amount_out[coin];
+        amount_out_lp[coin] = new web3.utils.BN(await curvePoolContract.methods.calc_token_amount(currency_in, true).call());
+        console.log(`Should yield ${web3.utils.fromWei(amount_out[coin], currency_fromWei_map[coin])} ${coin} (${web3.utils.fromWei(amount_out_lp[coin], lp_fromWei)} tokens)`);
+    }));
+
+    const highest = _.reduce(amount_out_lp, (max, lp, coin) => (lp.gt(max.lp) ? { coin: coin, lp: lp } : max), { coin: '', lp: 0 });
+    console.log(`Highest is ${highest.coin}`);
+
+    const deadline = Math.floor(DateTime.now().plus({ minutes: 1 }).toSeconds());
+    if(claim) {
+        return new Promise((resolve, reject) => {
+            sushiRouterContract.methods
+                .swapExactTokensForTokens(wmatic,
+                                            (new web3.utils.BN(amount_out[highest.coin]))
+                                                .mul(new web3.utils.BN('99'))
+                                                .div(new web3.utils.BN('100')),
+                                            [WMATIC_TOKEN, currency_token_map[highest.coin]],
+                                            MY_ACCOUNT_ID,
+                                            deadline
+                                        )
+                .send()
+            .on('transactionHash', hash => {
+                console.log(`Sushi Tx hash: '${hash}'`);
+            })
+            .on('confirmation', (number, receipt) => {
+                if(number == 0) { console.log(`Sushi confirmation number: ${number}`); }
+            })
+            .on('receipt', receipt => {
+                resolve(highest.coin);
+            })
+            .on('error', (err, receipt) => {
+                reject(err);
+            });
+        });
+    }
+};
+
+const depositToCurve = async (claim = true) => {
+    const USDT_decimals = await USDTContract.methods.decimals().call();
+    const USDT_fromWei = _.findKey(web3.utils.unitMap,
+                            v => v == (new web3.utils.BN(10).pow(new web3.utils.BN(USDT_decimals))).toString());
+
+    const USDC_decimals = await USDCContract.methods.decimals().call();
+    const USDC_fromWei = _.findKey(web3.utils.unitMap,
+                            v => v == (new web3.utils.BN(10).pow(new web3.utils.BN(USDC_decimals))).toString());
+
+    const DAI_decimals = await DAIContract.methods.decimals().call();
+    const DAI_fromWei = _.findKey(web3.utils.unitMap,
+                            v => v == (new web3.utils.BN(10).pow(new web3.utils.BN(DAI_decimals))).toString());
+
+    const lp_decimals = await curveLPContract.methods.decimals().call();
+    const lp_fromWei = _.findKey(web3.utils.unitMap,
+                            v => v == (new web3.utils.BN(10).pow(new web3.utils.BN(lp_decimals))).toString());
+
+    const coin_indices = _.range(3);
+    const underlying_coins = await Promise.all(_.map(coin_indices, i => curvePoolContract.methods.underlying_coins(i).call()));
+
+    const currency_token_map = {
+        USDC: USDC_TOKEN,
+        USDT: USDT_TOKEN,
+        DAI: DAI_TOKEN,
+    };
+
+    const currency_contract_map = {
+        USDC: USDCContract,
+        USDT: USDTContract,
+        DAI: DAIContract,
+    };
+
+    const currency_fromWei_map = {
+        USDC: USDC_fromWei,
+        USDT: USDT_fromWei,
+        DAI: DAI_fromWei,
+    };
+
+    const underlying_coin_indices = _.mapValues(currency_token_map, c => _.findIndex(underlying_coins, x => x == c));
+    const index_to_coin = _.invert(underlying_coin_indices);
+
+    const account_amounts = {};
+    await Promise.all(_.map(currency_contract_map, async (contract, coin) => {
+        account_amounts[coin] = await contract.methods.balanceOf(MY_ACCOUNT_ID).call();
+        console.log(`Account has balance of ${web3.utils.fromWei(account_amounts[coin], currency_fromWei_map[coin])} ${coin}`);
+    }));
+
+    const all_three_coins = [
+                                account_amounts[index_to_coin[0]],
+                                account_amounts[index_to_coin[1]],
+                                account_amounts[index_to_coin[2]],
+                            ];
+
+    const lpAmount = await curvePoolContract.methods.calc_token_amount(all_three_coins, true).call();
+
+    console.log(`Expect to get ${web3.utils.fromWei(lpAmount, lp_fromWei)} total LP tokens`);
+
+    if(claim) {
+        return new Promise((resolve, reject) => {
+            curvePoolContract.methods
+                .add_liquidity(all_three_coins,
+                               (new web3.utils.BN(lpAmount))
+                                .mul(new web3.utils.BN('99'))
+                                .div(new web3.utils.BN('100')),
+                               true
+                            )
+                .send()
+            .on('transactionHash', hash => {
+                console.log(`Curve deposit Tx hash: '${hash}'`);
+            })
+            .on('confirmation', (number, receipt) => {
+                if(number == 0) { console.log(`Curve deposit confirmation number: ${number}`); }
+            })
+            .on('receipt', receipt => {
+                resolve(true);
+            })
+            .on('error', (err, receipt) => {
+                reject(err);
+            });
+        });
+    }
+};
+
+const stakeCurve = async (claim = true) => {
+    const lpAmount = await curveLPContract.methods.balanceOf(MY_ACCOUNT_ID).call();
+    const lp_decimals = await curveLPContract.methods.decimals().call();
+    const lp_fromWei = _.findKey(web3.utils.unitMap,
+                            v => v == (new web3.utils.BN(10).pow(new web3.utils.BN(lp_decimals))).toString());
+
+    console.log(`Currently holding ${web3.utils.fromWei(lpAmount, lp_fromWei)} tokens`);
+
+    if(claim) {
+        return new Promise((resolve, reject) => {
+            curveGaugeContract.methods
+                .deposit(lpAmount)
+                .send()
+            .on('transactionHash', hash => {
+                console.log(`Curve stake Tx hash: '${hash}'`);
+            })
+            .on('confirmation', (number, receipt) => {
+                if(number == 0) { console.log(`Curve stake confirmation number: ${number}`); }
+            })
+            .on('receipt', receipt => {
+                resolve(true);
+            })
+            .on('error', (err, receipt) => {
+                reject(err);
+            });
+        });
+    }
+};
+
+(async () => {
+    const WALLET_SECRET_KEY = await getPassword({ account: 'Ethereum', service: 'Ethereum Wallet Private Key' });
+
+    const MY_ACCOUNT = web3.eth.accounts.privateKeyToAccount(WALLET_SECRET_KEY);
+    web3.eth.accounts.wallet.add(MY_ACCOUNT);
+    MY_ACCOUNT_ID = MY_ACCOUNT.address;
+
+    curveGaugeContract.options.from = MY_ACCOUNT_ID;
+    curveGaugeContract.options.gasPrice = web3.utils.toWei('1', 'gwei');
+    curveGaugeContract.options.gas = 1000000;
+    aaveIncentivesControllerContract.options.from = MY_ACCOUNT_ID;
+    aaveIncentivesControllerContract.options.gasPrice = web3.utils.toWei('1', 'gwei');
+    aaveIncentivesControllerContract.options.gas = 1000000;
+    sushiRouterContract.options.from = MY_ACCOUNT_ID;
+    sushiRouterContract.options.gasPrice = web3.utils.toWei('1', 'gwei');
+    sushiRouterContract.options.gas = 1000000;
+    curvePoolContract.options.from = MY_ACCOUNT_ID;
+    curvePoolContract.options.gasPrice = web3.utils.toWei('1', 'gwei');
+    curvePoolContract.options.gas = 1000000;
+
+    let wmatic = new web3.utils.BN(await claimCurve(nconf.get('x')));
+    wmatic = wmatic.add(new web3.utils.BN(await claimAave(nconf.get('x'))));
+
+    await swapToStablecoin(nconf.get('x') ? '0' : wmatic, nconf.get('x'));
+    await depositToCurve(nconf.get('x'));
+    await stakeCurve(nconf.get('x'));
+
+    process.exit(0);
+})();
